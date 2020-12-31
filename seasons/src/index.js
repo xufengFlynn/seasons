bea62aa822e120f5563a5f2ee09c21ef3a469793
xufengFlynn <zhangxufeng26@gmail.com>
2020-12-30 19:38:23 -0800

    optimized conditions in render method

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -18,8 +18,7 @@ class App extends React.Component {
     console.log("My component was just updated - it rerendered!");
   }
 
-  // React says we have to define render!!
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -28,7 +27,12 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  // React says we have to define render!!
+  render() {
+    return <div>{this.renderContent()}</div>;
   }
 }
 
